57a21e8fd98653994a7191f09e3c0e623cfc1a19
cyril <cyril.cermak@gmail.com>
2016-03-09 12:03:37 +0100

    Initial Commit

---
+++ Semestralka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Semestralka
+//
+//  Created by Cyril on 09/03/16.
+//  Copyright © 2016 cyril. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
